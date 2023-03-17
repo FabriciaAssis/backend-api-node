fb6cf8185bc0ff24156dbc8a9ed563bb057c00c5
Fabricia Assis <fabricia.assis@aluno.ifsp.edu.br>
2023-03-17 15:06:22 -0300

    add show course route

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -9,10 +9,21 @@ export const listAllCourses = (req, res) => {
   })
 }
 
+export const showCourse = (req, res) => {
+  const id = req.params.id
+
+  courseModel.showCourse(id, (error, result) => {
+    if (error)
+      res.status(500).json({ message: "Erro no Banco de Dados" })
+    if (result)
+      res.json(result)
+  })
+}
+
 export const createCourse = (req, res) => {
   const course = req.body
   //TODO Verificar se os dados são válidos
-  newCourse(course, (error, result) => {
+  courseModel.createCourse(course, (error, result) => {
     if (error)
       res.status(500).json({ message: "Erro no Banco de Dados" })
     if (result)
@@ -32,8 +43,25 @@ export const deleteCourse = (req, res) => {
   })
 }
 
+export const deleteIdCourse = (req, res) => {
+  const { id, slug } = req.params
+  console.log(slug)
+  //TODO Verificar se os dados são válidos
+  courseModel.deleteCourse(id, (error, result) => {
+    if (error)
+      res.status(500).json({ message: "Erro no Banco de Dados" })
+    if (result)
+      //TODO Verificar se ao menos uma linha foi removida!
+      res.json({ message: "Curso Deletado com Sucesso!" })
+  })
+}
+
 export const updateCourse = (req, res) => {
   const course = req.body
+
+  const varteste = req.query
+  console.log(varteste)
+
   //TODO Verificar se os dados são válidos
   courseModel.updateCourse(course, (error, result) => {
     if (error)
