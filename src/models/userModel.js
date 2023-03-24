c660adc751774c3e0a7600be82db46ce896ba304
Fabricia Assis <fabricia.assis@aluno.ifsp.edu.br>
2023-03-24 17:48:16 -0300

    create user schema to validate user

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,4 +1,21 @@
 import con from '../db/dbConnection.js'
+import { z } from 'zod'
+
+const userSchema = z.object({
+  id: z.number().optional(),
+  nome: z.string().min(3).max(50),
+  email:
+    z.string({ message: "Email Inválido" })
+      .email({ message: "Email Inválido" })
+      .min(5, { message: "O email deve ter ao menos 5 Caracteres" })
+      .max(50),
+  senha: z.string().min(3).max(50),
+  avatar: z.string()
+})
+
+export const validateUser = (user) => {
+  return userSchema.parse(user)
+}
 
 export const listAllUsers = (callback) => {
   const sql = "SELECT * FROM usuarios;"
@@ -11,7 +28,6 @@ export const listAllUsers = (callback) => {
     }
   })
 }
-
 export const showUser = (id, callback) => {
   const sql = "SELECT * FROM usuarios WHERE id = ?;"
   const value = [id]
@@ -24,13 +40,10 @@ export const showUser = (id, callback) => {
     }
   })
 }
-
 export const createUser = (user, callback) => {
   const { nome, email, senha, avatar } = user
-
   const sql = 'INSERT INTO usuarios (nome, email, senha, avatar) VALUES (?, ?, ?, ?);'
   const values = [nome, email, senha, avatar]
-
   con.query(sql, values, (err, result) => {
     if (err) {
       callback(err, null)
@@ -40,7 +53,6 @@ export const createUser = (user, callback) => {
     }
   })
 }
-
 export const deleteUser = (id, callback) => {
   const sql = 'DELETE FROM usuarios WHERE id = ?;'
   const value = [id]
@@ -53,12 +65,10 @@ export const deleteUser = (id, callback) => {
     }
   })
 }
-
 export const updateUser = (user, callback) => {
   const { id, nome, email, senha, avatar } = user
   const sql = 'UPDATE usuarios SET nome = ?, email = ?, senha = ?, avatar = ?  WHERE id = ? ;'
   const values = [nome, email, senha, avatar, id]
-
   con.query(sql, values, (err, result) => {
     if (err) {
       callback(err, null)
@@ -69,4 +79,4 @@ export const updateUser = (user, callback) => {
   })
 }
 
-export default { listAllUsers, showUser, createUser, deleteUser, updateUser }
+export default { listAllUsers, showUser, createUser, deleteUser, updateUser, validateUser }
